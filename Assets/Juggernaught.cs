7fc437c43b52a5be6c7e95d608fe6794e4399e00
ArgentWaropod <62075349+ArgentWaropod@users.noreply.github.com>
2020-10-05 11:04:48 -0400

    Zombies + PerkBase

---
+++ Assets/Juggernaught.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juggernaught : PerkMachine
+{
+    public void Start()
+    {
+        cost = 2500;
+        perkName = "Juggernaught";
+        purchased = false;
+        active = false;
+        canvas.gameObject.SetActive(false);
+    }
+
+    public override void OnPurchase(Manager thePlayer)
+    {
+        thePlayer.playerHealth = 5;
+    }
+}
---
+++ Assets/PerkMachine.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerkMachine : MonoBehaviour
+{
+    public GameObject canvas;
+    public TextMeshProUGUI field;
+    public int cost;
+    public string perkName;
+    public bool purchased;
+    public bool active;
+    public Manager man;
+    GameObject player;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        purchased = false;
+        active = false;
+        canvas.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("SPACE");
+            if (active)
+            {
+                man.points -= cost;
+                canvas.gameObject.SetActive(false);
+                active = false;
+                purchased = true;
+                OnPurchase(man);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player" && !purchased)
+        {
+            player = other.gameObject;
+            canvas.gameObject.SetActive(true);
+            field.text = "Buy " + perkName + " for " + cost.ToString() + "?";
+            active = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canvas.gameObject.SetActive(false);
+        active = false;
+    }
+    public virtual void OnPurchase(Manager thePlayer)
+    {
+
+    }
+}
